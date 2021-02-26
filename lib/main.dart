89e6a29e779bc416c44ea3eba13eebf22bdefe77
janWICKEDpro <janroyal45@gmail.com>
2021-02-26 16:37:20 +0100

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(FireApp());
 }
+
+class FireApp extends StatefulWidget{
+  @override
+  State<FireApp> createState() {
+    FireAppState();
+  }
+}
+
+class FireAppState extends State<FireApp>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+
+  }
+
+}

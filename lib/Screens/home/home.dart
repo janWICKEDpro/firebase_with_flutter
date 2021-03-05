7cd3c889a99c4184447595e321bca985dc689fe0
janWICKEDpro <janroyal45@gmail.com>
2021-03-05 02:34:30 +0100

    creating documents

--- lib/Screens/home/home.dart
+++ lib/Screens/home/home.dart
@@ -1,11 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app/services/auth.dart';
+import 'package:firebase_app/services/database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'brew_list.dart';
 
 class Home extends StatelessWidget {
  final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child:Scaffold(
       backgroundColor: Colors.brown[50],
       appBar: AppBar(
         title: Text("Brew Crew"),
@@ -19,6 +25,8 @@ class Home extends StatelessWidget {
           )
         ],
       ),
+        body: BrewList(),
+    )
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -4,10 +4,14 @@ class DatabaseService{
 
   final String uid;
   DatabaseService({this.uid});
-
   final CollectionReference brewCollection= FirebaseFirestore.instance.collection("brews");
 
-  //
+  //streams
+  Stream<QuerySnapshot> get brews{
+    return brewCollection.snapshots();
+  }
+
+  //update
 Future updateUserData(String sugars, String name, int strength) async{
   return await brewCollection.doc(uid).set(
       {

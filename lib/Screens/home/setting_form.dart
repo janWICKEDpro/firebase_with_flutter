5a6abfb976b1ce937e6fcab7202c42a0f81904f7
janWICKEDpro <janroyal45@gmail.com>
2021-03-13 00:30:06 +0100

    almost done

--- lib/Screens/home/setting_form.dart
+++ lib/Screens/home/setting_form.dart
@@ -11,7 +11,7 @@ class Settingss extends StatefulWidget {
 
 class _SettingsState extends State<Settingss> {
   final _key = GlobalKey<FormState>();
-  List<String> sugars = ['0','1''2','3'];
+  List<String> sugars = ['0','1','2','3'];
 
   String _currentName;
   String _currentSugars;
@@ -23,7 +23,7 @@ class _SettingsState extends State<Settingss> {
         stream: DatabaseService(uid:user.uid).userData,
         builder: (context, snapshot) {
           if(snapshot.hasData) {
-            final user =snapshot.data;
+            final users =snapshot.data;
             return Form(
               key: _key,
               child: Column(
@@ -32,13 +32,13 @@ class _SettingsState extends State<Settingss> {
                     height: 20,
                   ),
                   TextFormField(
-                    initialValue: user.name,
+                    initialValue: users.name,
                     validator: (val) => val.length < 6 ? 'beef' : null,
                     onChanged: (val) => setState(() => _currentName = val),
                   ),
                   //Dropdown
                   DropdownButtonFormField(
-                    value: _currentSugars ?? user.sugars,
+                    value: _currentSugars ?? users.sugars,
                     items: sugars.map((sugar) {
                       return DropdownMenuItem(
                         value: sugar,
@@ -49,24 +49,27 @@ class _SettingsState extends State<Settingss> {
                   ),
                   //slider
                   Slider(
-                    value: (_currentStrength ?? user.strength).toDouble(),
-                    activeColor: Colors.brown[_currentStrength ?? user.strength],
-                    inactiveColor: Colors.brown[_currentStrength ?? user.strength],
+                    divisions: 8,
+                    value: (_currentStrength ?? users.strength).toDouble(),
+                    activeColor: Colors.brown[_currentStrength ?? users.strength],
+                    inactiveColor: Colors.brown[_currentStrength ?? users.strength],
                     min: 100,
                     max: 900,
                     onChanged: (val) =>
                         setState(() => _currentStrength = val.round()),
                   ),
-                  RaisedButton(onPressed: () async{
+                  TextButton(onPressed: () async{
                     if(_key.currentState.validate()){
-                      await DatabaseService(uid: user.uid).updateUserData(_currentSugars ?? user.sugars, _currentName ?? user.name, _currentStrength ?? user.strength);
+                      await DatabaseService(uid: user.uid).updateUserData(_currentSugars ?? users.sugars, _currentName ?? users.name, _currentStrength ?? users.strength);
+                      Navigator.pop(context);
+                      print(_currentStrength);
                     }
-                    Navigator.pop(context);
+
                   },
                     child: Text(
                         "Submit"
                     ),
-                    color: Colors.pink,
+
                   )
                 ],
               ),
